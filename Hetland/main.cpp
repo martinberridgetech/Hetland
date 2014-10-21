6151a6625144494e64994ff57d32bc3a09d46c81
martinberridgetech <martin.berridge@gmail.com>
2014-10-21 20:18:53 +0100

    Initial stub to commit test cases

--- Hetland/main.cpp
+++ Hetland/main.cpp
@@ -6,13 +6,26 @@
 //  Copyright (c) 2014 berridge martin. All rights reserved.
 //
 
-#include <iostream>
+#define  BOOST_TEST_MODULE Suites
 
-int main(int argc, const char * argv[])
+#include <boost/test/included/unit_test.hpp>
+/*
+ int main(int argc, const char * argv[])
+ {
+ 
+ // insert code here...
+ std::cout << "Hello, World!\n";
+ return 0;
+ }
+ */
+
+
+BOOST_AUTO_TEST_SUITE(mitstack)
+
+
+BOOST_AUTO_TEST_CASE(mitstack)
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    BOOST_CHECK_MESSAGE(false, "Everything's OK really");
 }
 
+BOOST_AUTO_TEST_SUITE_END()
