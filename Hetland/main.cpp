a92cf86c19999486c1b4ff9998be2d5b3bf324c8
martinberridgetech <martin.berridge@gmail.com>
2014-10-21 19:42:35 +0100

    Initial Commit

---
+++ Hetland/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hetland
+//
+//  Created by berridge martin on 10/21/14.
+//  Copyright (c) 2014 berridge martin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
